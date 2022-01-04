eeeb424f7a1eac57a0797e18e7539d50a4c75700
MattTheCoder-W <matigdynia@hotmail.com>
2022-01-04 20:29:38 +0100

    Updated wallpapers, added picom

--- autostart/dwm-autostart.sh
+++ autostart/dwm-autostart.sh
@@ -4,12 +4,17 @@
 ~/.screenlayout/rightvert.sh
 
 # Set wallpaper
-WP="/home/m4t1/.local/share/wallpapers/wall.png"
-feh --bg-tile $WP --bg-fill $WP
+WP_DIR="/home/m4t1/.local/share/wallpapers"
+LEFT="4k-wall.jpg"
+RIGHT="wall.png"
+feh --bg-tile $WP_DIR/$LEFT --bg-fill $WP_DIR/$RIGHT
 
 # Startup volume
 pactl set-sink-volume 0 20%
 
 # Startup bar
 /usr/local/share/dwm/sbar &
+
+# Transparency
+/usr/bin/picom -b
 
