fea336a8cddf977b02170d0f409860d26e867efc
MattTheCoder-W <matigdynia@hotmail.com>
2022-01-04 19:42:26 +0100

    Refresh bar on volume change

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -12,9 +12,13 @@ if [[ "$action" == "up" ]]; then
 	/usr/bin/pactl set-sink-volume 0 +5%
 	if [[ $(get_vol) -gt 100 ]]; then
 		/usr/bin/pactl set-sink-volume 0 100%
+	else
+		/usr/local/share/dwm/refbar
 	fi
 elif [[ "$action" == "down" ]]; then
 	/usr/bin/pactl set-sink-volume 0 -5%
+	/usr/local/share/dwm/refbar
 elif [[ "$action" == "mute" ]]; then
 	/usr/bin/pactl set-sink-mute 0 toggle
+	/usr/local/share/dwm/refbar
 fi
