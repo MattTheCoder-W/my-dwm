464afe935516ebc005c68753eb64278b048f4993
MattTheCoder-W <matigdynia@hotmail.com>
2022-01-09 11:47:24 +0100

    Monitor conf for two vertical

--- autostart/rightvert.sh
+++ autostart/rightvert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-2 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 1025x0 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-D-0 --off
